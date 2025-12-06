a223c809c13f3eefa85479fcbbce991c5712bc9b
Rahul-ig <rahulshekhawat211@gmail.com>
2025-12-06 21:46:32 +0530

    first commit

--- tourism_project/model_building/data_registration.py
+++ tourism_project/model_building/data_registration.py
@@ -1,18 +1,28 @@
+from huggingface_hub.utils import RepositoryNotFoundError, HfHubHTTPError
 from datasets import load_dataset, Dataset
 from huggingface_hub import HfApi
-import pandas as pd
 import os
 
-repo_type = "dataset"
+# Initialize HuggingFace API
+api = HfApi(token=os.getenv("HF_TOKEN"))
+
+# Define repository details
 repo_id = "RahulSingh211/tourism_dataset"
+repo_type = "dataset"
 
-api = HfApi(token=os.getenv('HF_TOKEN'))
-
-
-# Push to Hugging Face Hub
+# Create repository if it doesn't exist
 api.create_repo(
-    repo_id=repo_id, repo_type=repo_type, exist_ok=True
+    repo_id=repo_id, 
+    repo_type=repo_type, 
+    private=False,
+    exist_ok=True
 )
 print(f"Repository '{repo_id}' is ready.")
 
-api.upload_folder(folder_path="tourism_project/data", repo_id=repo_id, repo_type=repo_type)
+# Upload the tourism.csv file to the repository
+print("Uploading tourism.csv to Hugging Face Hub...")
+api.upload_folder(
+    folder_path="tourism_project/data",
+    repo_id=repo_id,
+    repo_type=repo_type
+)
